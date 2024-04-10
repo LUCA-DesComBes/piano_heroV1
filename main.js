6d015b3ca40d52c8cffb6375c4a7b0ee719e33b8
LUCA-DesComBes <ldescombes@edenschool.fr>
2024-04-10 09:50:44 +0200

    black&white

---
+++ main.js
@@ -0,0 +1,9 @@
+const container = document.createElement("div")
+container.classList = "container";
+document.body.appendChild(container)
+const white = document.createElement("div");
+white.classList = "white";
+container.appendChild(white);
+const black = document.createElement("div");
+black.classList = "black";
+white.appendChild(black);
