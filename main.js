438b7b4e8c445e7e5181f69c853e1db4e76ad292
LUCA-DesComBes <ldescombes@edenschool.fr>
2024-04-10 10:03:24 +0200

    click aqua

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const container = document.createElement("div")
 container.classList = "container";
 document.body.appendChild(container)
-const white = document.createElement("div");
-white.classList = "white";
-container.appendChild(white);
-const black = document.createElement("div");
-black.classList = "black";
-white.appendChild(black);
+
+
+for(let i = 0; i < 10; i++){
+    const white = document.createElement("div");
+    white.classList = "white";
+    container.appendChild(white);
+    const black = document.createElement("div");
+    black.classList = "black";
+    white.appendChild(black);
+    white.addEventListener("click", () => {
+        white.style.backgroundColor = "aqua"
+    })
+}
